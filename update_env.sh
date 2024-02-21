c6b813434a5f9c24fd8c737e641277a099135476
AchrafKhGit <kharbabachraf@gmail.com>
2024-02-21 12:12:32 +0100

    new modification

--- update_env.sh
+++ update_env.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-# Get the IP address of the wlo1 interface
-ip_address=$(ip addr show wlo1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+# Check if wlo1 exists, otherwise use wlp0s20f3
+if ip link show wlo1 &> /dev/null; then
+  interface="wlo1"
+elif ip link show wlp0s20f3 &> /dev/null; then
+  interface="wlp0s20f3"
+else
+  echo "Neither wlo1 nor wlp0s20f3 found."
+  exit 1
+fi
+
+# Get the IP address of the selected interface
+ip_address=$(ip addr show $interface | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 
 # Update the .env file with the IP address
 sed -i "s#^EXPO_PUBLIC_BASE_URL=.*#EXPO_PUBLIC_BASE_URL=http://$ip_address:8000#" .env
